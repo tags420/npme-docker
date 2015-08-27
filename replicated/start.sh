f96c0b33f91aa7f49d4967ad85d1a62a6dbb1a33
Marc Campbell <marc.e.campbell@gmail.com>
2015-08-27 22:13:46 +0000

    WORKDIR is set, no need for this cd

--- replicated/start.sh
+++ replicated/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 chown -R couchdb:couchdb /etc/npme/couchdb
-cd /etc/npme
 ndm generate --uid=root --gid=root --platform=initd && service redis-server start | service nginx start | couchdb | npme restart | tail -f ./logs/*
 
