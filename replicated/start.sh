b25b85e4d1308bbf8a81019fe223b29c35552b0e
Marc Campbell <marc.e.campbell@gmail.com>
2015-08-28 17:09:12 +0000

    setting up cron to keep policy follower up and running

--- replicated/start.sh
+++ replicated/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chown -R couchdb:couchdb /etc/npme/couchdb
+/etc/npme/bin/install-couch-app.sh
+
 ndm generate --uid=root --gid=root --platform=initd && service redis-server start | service nginx start | couchdb | npme restart | tail -f ./logs/*
 
