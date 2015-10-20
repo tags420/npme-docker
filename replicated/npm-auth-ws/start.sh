967b9255b771124aa6b0e9e5ab983cd96654a5f4
bcoe <bencoe@gmail.com>
2015-10-20 15:11:25 -0700

    slight refactor based on @aredridel's code review

--- replicated/npm-auth-ws/start.sh
+++ replicated/npm-auth-ws/start.sh
@@ -2,7 +2,9 @@
 cd /etc/npme/node_modules/@npm/npm-auth-ws/
 
 if [ -n "$GITHUB_ORG" ]; then
-  node ./bin/npm-auth-ws.js start --front-door-host=$FRONT_DOOR_HOST --github-host=$GITHUB_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --authentication-method=$AUTHENTICATION_METHOD --authorization-method=$AUTHORIZATION_METHOD --session-handler=$SESSION_HANDLER --reject-unauthorized=$REJECT_UNAUTHORIZED --port=5000 --host=0.0.0.0 --github-org=$GITHUB_ORG
+  ORG_FLAG=--github-org="$GITHUB_ORG"
 else
-  node ./bin/npm-auth-ws.js start --front-door-host=$FRONT_DOOR_HOST --github-host=$GITHUB_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --authentication-method=$AUTHENTICATION_METHOD --authorization-method=$AUTHORIZATION_METHOD --session-handler=$SESSION_HANDLER --reject-unauthorized=$REJECT_UNAUTHORIZED --port=5000 --host=0.0.0.0
+  ORG_FLAG=
 fi
+
+node ./bin/npm-auth-ws.js start --front-door-host=$FRONT_DOOR_HOST --github-host=$GITHUB_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --authentication-method=$AUTHENTICATION_METHOD --authorization-method=$AUTHORIZATION_METHOD --session-handler=$SESSION_HANDLER --reject-unauthorized=$REJECT_UNAUTHORIZED --port=5000 --host=0.0.0.0 $ORG_FLAG
