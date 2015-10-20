e7ab5f571415c1026f226de71be13ec1a8263c90
bcoe <bencoe@gmail.com>
2015-10-20 14:55:13 -0700

    optionally lock down publishes to a single GitHub org

--- replicated/npm-auth-ws/start.sh
+++ replicated/npm-auth-ws/start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 cd /etc/npme/node_modules/@npm/npm-auth-ws/
-node ./bin/npm-auth-ws.js start --front-door-host=$FRONT_DOOR_HOST --github-host=$GITHUB_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --authentication-method=$AUTHENTICATION_METHOD --authorization-method=$AUTHORIZATION_METHOD --session-handler=$SESSION_HANDLER --reject-unauthorized=$REJECT_UNAUTHORIZED --port=5000 --host=0.0.0.0
+
+if [ -n "$GITHUB_ORG" ]; then
+  node ./bin/npm-auth-ws.js start --front-door-host=$FRONT_DOOR_HOST --github-host=$GITHUB_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --authentication-method=$AUTHENTICATION_METHOD --authorization-method=$AUTHORIZATION_METHOD --session-handler=$SESSION_HANDLER --reject-unauthorized=$REJECT_UNAUTHORIZED --port=5000 --host=0.0.0.0 --github-org=$GITHUB_ORG
+else
+  node ./bin/npm-auth-ws.js start --front-door-host=$FRONT_DOOR_HOST --github-host=$GITHUB_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --authentication-method=$AUTHENTICATION_METHOD --authorization-method=$AUTHORIZATION_METHOD --session-handler=$SESSION_HANDLER --reject-unauthorized=$REJECT_UNAUTHORIZED --port=5000 --host=0.0.0.0
+fi
