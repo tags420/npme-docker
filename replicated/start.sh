2d5147fda46b7a38867a681054ed9e26592ea181
Benjamin Coe <ben@npmjs.com>
2015-11-16 21:16:59 -0800

    fix configuration for read-through-cache

--- replicated/start.sh
+++ replicated/start.sh
@@ -10,7 +10,7 @@ fi
 
 cd /etc/npme/node_modules/@npm/registry-frontdoor/
 if [ -n "$PROXY_URL" ]; then
-  node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --proxy=$PROXY_URL --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --front-door-host=$FRONT_DOOR_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=8080 --host=0.0.0.0
+  node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --proxy=$PROXY_URL --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=8080 --host=0.0.0.0
 else
-  node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --front-door-host=$FRONT_DOOR_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=8080 --host=0.0.0.0
+  node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=8080 --host=0.0.0.0
 fi
