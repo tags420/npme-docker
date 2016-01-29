64c39f2edaba08c7fd458068569d2c251d858b6d
Benjamin Coe <ben@npmjs.com>
2016-01-28 17:32:18 -0800

    always symlink the directory.

--- replicated/newww/start.sh
+++ replicated/newww/start.sh
@@ -3,8 +3,9 @@
 if [ ! -d "/etc/npme/data/npm-explicit-installs" ]; then
   mv /etc/npme/node_modules/newww/node_modules/npm-explicit-installs /etc/npme/data/npm-explicit-installs
   cd /etc/npme/data/npm-explicit-installs; npm i --production
-  ln -s /etc/npme/data/npm-explicit-installs /etc/npme/node_modules/newww/node_modules/npm-explicit-installs
 fi
+rm -rf /etc/npme/node_modules/newww/node_modules/npm-explicit-installs
+ln -f -s /etc/npme/data/npm-explicit-installs /etc/npme/node_modules/newww/node_modules
 
 { cd /etc/npme/node_modules/newww; node server.js; } &
 { cd /etc/npme/node_modules/@npm/npmo-auth-callbacks; node bin/npmo-auth-callbacks.js start --certificate=$CERTIFICATE --redis=$REDIS_URL --entity-id=$ENTITY_ID --assert-endpoint=$ASSERT_ENDPOINT --logout-endpoint=$LOGOUT_ENDPOINT --nameid-format=$NAMEID_FORMAT --sso-login-url=$SSO_LOGIN_URL --sso-logout-url=$SSO_LOGOUT_URL; } &
