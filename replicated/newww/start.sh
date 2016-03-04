23fc6fd6b67f3462d1bb14ce8bd0be711802b733
Benjamin Coe <ben@npmjs.com>
2016-03-04 14:42:14 -0800

    add auth to website

--- replicated/newww/start.sh
+++ replicated/newww/start.sh
@@ -10,6 +10,7 @@ fi
 rm -rf /etc/npme/node_modules/newww/node_modules/npm-explicit-installs
 ln -f -s /etc/npme/data/npm-explicit-installs /etc/npme/node_modules/newww/node_modules
 
+{ cd /etc/npme/node_modules/@npm/npmo-web-proxy; node ./bin/npmo-web-proxy.js start; } &
 { cd /etc/npme/node_modules/newww; node server.js; } &
 { cd /etc/npme/node_modules/@npm/npmo-auth-callbacks; node bin/npmo-auth-callbacks.js start --certificate=$CERTIFICATE --redis=$REDIS_URL --entity-id=$ENTITY_ID --assert-endpoint=$ASSERT_ENDPOINT --logout-endpoint=$LOGOUT_ENDPOINT --nameid-format=$NAMEID_FORMAT --sso-login-url=$SSO_LOGIN_URL --sso-logout-url=$SSO_LOGOUT_URL; } &
 wait -n
