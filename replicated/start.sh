5f399e7ae21654b84487871414acf27875317afb
Benjamin E. Coe <bencoe@gmail.com>
2016-07-13 12:52:52 -0700

    feat: rewrite tarball URLs, making server migration much easier (#87)
    * feat: rewrite tarball URLs, so that it's easier to migrate between servers
    
    * chore: bump the npme version #
    
    * fix: need to use bash rather than sh for advanced spawning behavior
    
    * fix: patched version of npme-couch-rewrite-proxy
    
    * specify port (so we can eventually override it via user settings)

--- replicated/start.sh
+++ replicated/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 chmod 755 /etc/npme/install-couch-app.sh
 
 curl --fail -XGET $COUCH_URL
@@ -17,9 +17,12 @@ if [ $AUTH_GLOBAL_PACKAGES == 'false' ]; then
   unset AUTH_GLOBAL_PACKAGES
 fi
 
-cd /etc/npme/node_modules/@npm/registry-frontdoor/
 if [ -n "$PROXY_URL" ]; then
-  node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --proxy=$PROXY_URL --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=8080 --host=0.0.0.0 --white-list-path=$WHITELIST_PATH --couch-url-cache=$COUCH_URL_REMOTE
+  { cd /etc/npme/node_modules/@npm/registry-frontdoor/; node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --proxy=$PROXY_URL --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=9999 --host=0.0.0.0 --white-list-path=$WHITELIST_PATH --couch-url-cache=$COUCH_URL_REMOTE; } &
 else
-  node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=8080 --host=0.0.0.0 --white-list-path=$WHITELIST_PATH --couch-url-cache=$COUCH_URL_REMOTE
+  { cd /etc/npme/node_modules/@npm/registry-frontdoor/; node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=9999 --host=0.0.0.0 --white-list-path=$WHITELIST_PATH --couch-url-cache=$COUCH_URL_REMOTE; } &
 fi
+{ cd /etc/npme/node_modules/@npm/couch-url-rewrite-proxy/; node ./bin/couch-url-rewrite-proxy.js start --port 8080; } &
+wait -n
+kill 0
+exit 1
