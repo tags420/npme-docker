23d4a26783fcf2169957ad7277ed8da2bfe51680
Benjamin E. Coe <bencoe@gmail.com>
2016-09-23 14:07:28 -0700

    feat: environment variable only version of npme container (#98)
    * feat: environment variable only version of npme container
    
    * chore: bump container version
    
    * fix: modified script based on code-review
    
    * fix: remove duplicate COUCH_URL based on @donny-dont's review

--- replicated/start.sh
+++ replicated/start.sh
@@ -17,6 +17,9 @@ if [ $AUTH_GLOBAL_PACKAGES == 'false' ]; then
   unset AUTH_GLOBAL_PACKAGES
 fi
 
+# copy the license to disk from environment.
+echo $LICENSE_FIELD > /etc/npme/.license.json
+
 if [ -n "$PROXY_URL" ]; then
   { cd /etc/npme/node_modules/@npm/registry-frontdoor/; node ./bin/registry-frontdoor.js start --binary-directory=$BINARY_DIRECTORY --binaries-host=$BINARIES_HOST --proxy=$PROXY_URL --auth-host=$AUTH_HOST --reject-unauthorized=$REJECT_UNAUTHORIZED --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --validate-host=$VALIDATE_HOST --front-door-host=$FRONT_DOOR_HOST --read-through-cache=$READ_THROUGH_CACHE --auth-fetch=$AUTH_FETCH --port=9999 --host=0.0.0.0 --white-list-path=$WHITELIST_PATH --couch-url-cache=$COUCH_URL_REMOTE; } &
 else
