9f2724f8cb64ef44f5d0b33a25c12808deda998a
Andrew Goode <andrew@npmjs.com>
2017-01-25 17:01:16 -0500

    fix: use proxy for npme add-package (#109)
    * fix: use proxy for npme add-package
    
    also pull policy-follower from docker hub
    
    * whoops, admin commands must reference public image name
    
    * lets give this named container thing a try
    
    * cool, give each container a (somewhat) meaningful consistent name
    
    * prefix container names with npme- to be safe

--- replicated/policy-follower/add-package.sh
+++ replicated/policy-follower/add-package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 cd /etc/npme/node_modules/@npm/package-whitelist
-node ./bin/package-whitelist.js add-package $@ --white-list-path=$WHITELIST_PATH
+http_proxy=$PROXY_URL https_proxy=$PROXY_URL node ./bin/package-whitelist.js add-package $@ --white-list-path=$WHITELIST_PATH
