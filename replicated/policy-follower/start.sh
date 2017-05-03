7c98e3345f123db2b6dcb0f9ea4313bd5551caf7
Benjamin E. Coe <bencoe@gmail.com>
2017-05-03 13:48:54 -0700

    chore: upgrade policy follower (#115)

--- replicated/policy-follower/start.sh
+++ replicated/policy-follower/start.sh
@@ -7,7 +7,7 @@ fi
 
 cd /etc/npme/node_modules/@npm/policy-follower/
 if [ -n "$PROXY_URL" ]; then
-  node ./bin/policy-follower.js start --policy=$POLICY --front-door-host=$FRONT_DOOR_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --seq-file=$SEQ_FILE --validate-host=$VALIDATE_HOST --white-list-path=$WHITELIST_PATH --proxy=$PROXY_URL --send-shared-fetch-secret-to-remote-couch=$SEND_SHARED_FETCH_SECRET
+  node ./bin/policy-follower.js start --policy=$POLICY --front-door-host=$FRONT_DOOR_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --seq-file=$SEQ_FILE --validate-host=$VALIDATE_HOST --white-list-path=$WHITELIST_PATH --proxy=$PROXY_URL --send-shared-fetch-secret-to-remote-couch=$SEND_SHARED_FETCH_SECRET --reject-unauthorized=$REJECT_UNAUTHORIZED
 else
-  node ./bin/policy-follower.js start --policy=$POLICY --front-door-host=$FRONT_DOOR_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --seq-file=$SEQ_FILE --validate-host=$VALIDATE_HOST --white-list-path=$WHITELIST_PATH --send-shared-fetch-secret-to-remote-couch=$SEND_SHARED_FETCH_SECRET
+  node ./bin/policy-follower.js start --policy=$POLICY --front-door-host=$FRONT_DOOR_HOST --shared-fetch-secret=$SHARED_FETCH_SECRET --couch-url=$COUCH_URL --couch-url-remote=$COUCH_URL_REMOTE --seq-file=$SEQ_FILE --validate-host=$VALIDATE_HOST --white-list-path=$WHITELIST_PATH --send-shared-fetch-secret-to-remote-couch=$SEND_SHARED_FETCH_SECRET --reject-unauthorized=$REJECT_UNAUTHORIZED
 fi
